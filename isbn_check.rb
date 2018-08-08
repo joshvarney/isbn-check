42b95b4597490e4d6a2f1fa7906888cfd82bd898
Josh Varney <joshcainvarney@gmail.com>
2018-08-07 22:07:31 -0400

    test that can remove spaces and hyphens

--- isbn_check.rb
+++ isbn_check.rb
@@ -1,3 +1,32 @@
 def isbn_input(isbn)
-	isbn
+	alpha1 = ('A'..'W').to_a
+	alpha2 = ('Y'..'w').to_a
+	alpha3 = ('y'..'z').to_a
+	char1 = ('!'..',').to_a
+	char2 = ('.'..'/').to_a
+	char3 = (':'..'?').to_a
+	invalid_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
+	count = 0
+	isbn.split("").each { |x| invalid_char.each { |y|
+							if x == y
+								count += 1
+							end
+							}
+
+	}
+
+	count
+	isbn = isbn.split("")
+	isbn.delete_if {|x| x == " "}
+	isbn.delete_if {|x| x == "-"}
+	isbn = isbn.join
+	p isbn
 end
+
+def isbn10_check()
+	isbn10
+end
+
+def isbn13_check()
+	isbn13
+end	
--- isbn_test.rb
+++ isbn_test.rb
@@ -2,12 +2,24 @@ require 'minitest/autorun'
 require_relative 'isbn_check.rb'
 class Isbn_check_test < Minitest::Test
 
-	def test_that_takes_input
-		isbn = "2132"
-		assert_equal("2132", isbn_input(isbn))
-	end
-	def test_that_is_a_string
-		isbn = "123456"
-		assert_equal(String, isbn_input(isbn).class)
-	end
+	# def test_that_takes_input
+	# 	isbn = "2132"
+	# 	assert_equal("2132", isbn_input(isbn))
+	# end
+	# def test_that_is_a_string
+	# 	isbn = "123456"
+	# 	assert_equal(String, isbn_input(isbn).class)
+	# end
+	# def test_that_ivalid_char_count_works
+	# 	isbn = "xkue123_-  .,45X"
+	# 	assert_equal(6, isbn_input(isbn))
+	# end
+	def test_that_can_remove_spaces
+		isbn = " 56 12 45 8 7 9 4 x w o"
+		assert_equal("5612458794xwo", isbn_input(isbn))
+	end 
+	def test_that_can_remove_hyphens
+		isbn = "    -----5"
+		assert_equal("5",isbn_input(isbn))
+	end	
 end
