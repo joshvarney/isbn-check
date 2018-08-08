f3368e203bd3729471ec7cbb0bccf81f380acb6c
Josh Varney <joshcainvarney@gmail.com>
2018-08-08 00:55:06 -0400

    passed test to pop off check digit

--- isbn_check.rb
+++ isbn_check.rb
@@ -1,4 +1,4 @@
-def isbn_input(isbn)
+def invalid_char()
 	alpha1 = ('A'..'W').to_a
 	alpha2 = ('Y'..'w').to_a
 	alpha3 = ('y'..'z').to_a
@@ -6,6 +6,8 @@ def isbn_input(isbn)
 	char2 = ('.'..'/').to_a
 	char3 = (':'..'?').to_a
 	invalid_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
+end
+def isbn_input(isbn)
 	count = 0
 	count2 = 0
 	isbn = isbn.split("")
@@ -17,27 +19,28 @@ def isbn_input(isbn)
 						end
 						}
 	}
-	count
 	isbn.each { |char| ["x", "X"].each { |x|
 						if char == x
 							count2 +=1
 						end		
 						} 
 	}
-	if count >= 1 || count2 > 1
-		isbn = false
-	end
-	# if isbn[0] == "x" 	
-	unless isbn == false
-	isbn = isbn.join
-	end
+		if count >= 1 || count2 > 1
+			isbn = false
+		elsif isbn.count != 10 && isbn.count != 13
+			isbn = false
+		elsif isbn.count == 13 && count2 > 0
+			isbn = false
+		elsif isbn.last.downcase != "x" && isbn.include?("x") == true
+			isbn = false
+		else
+			isbn[-1] = isbn[-1].downcase 	
+			isbn = isbn.join
+		end	
 	isbn
 end
-
-def isbn10_check()
-	isbn10
-end
-
-def isbn13_check()
-	isbn13
+def isbn10_check(isbn10)
+	isbn10 = isbn10.split('')
+	check_digit10 = isbn10.pop
+	p isbn10.join
 end	
--- isbn_test.rb
+++ isbn_test.rb
@@ -15,19 +15,52 @@ class Isbn_check_test < Minitest::Test
 	# 	assert_equal(6, isbn_input(isbn))
 	# end
 	def test_that_isbn_false_if_invalidchar_included
-		isbn = "45 1213 *"
+		isbn = "45 1213 23*"
+		assert_equal(false, isbn_input(isbn))
+	end
+	def test_that_isbn_false_if_include_all_letters
+		isbn = "abscdelkjl"
 		assert_equal(false, isbn_input(isbn))
 	end
 	# def test_that_can_remove_spaces
 	# 	isbn = " 56 12 45 8 7 9 4 x w o"
 	# 	assert_equal("5612458794xwo", isbn_input(isbn))
 	# end 
-	def test_that_can_remove_hyphens
-		isbn = "    -----5"
-		assert_equal("5", isbn_input(isbn))
-	end
+	# def test_that_can_remove_hyphens
+	# 	isbn = "    -----5"
+	# 	assert_equal("5", isbn_input(isbn))
+	# end
 	# def test_that_if_x_is_included_and_count2_works
 	# 	isbn = "x X X 4564654---enrrt,mnx"
 	# 	assert_equal(4, isbn_input(isbn))
-	# end	
+	# end
+	def test_that_if_statement_works_for_10_digit_isbn_with_x
+		isbn = "123456789X"
+		assert_equal("123456789x", isbn_input(isbn))
+	end
+	def test_that_an_x_anywhere_but_the_begining_fails
+		isbn = "123x456789"
+		assert_equal(false, isbn_input(isbn))
+	end
+	def test_that_having_an_x_in_isbn13_fails
+		isbn = "123456789012x"
+		assert_equal(false, isbn_input(isbn))
+	end
+	def test_that_isbn_with_13_numbers_passes_all_numbers
+		isbn = "1234567890123"
+		assert_equal("1234567890123", isbn_input(isbn))
+	end	
+	# def test_that_isbn10_returns_an_array
+	# 	isbn10 = "123456789x"
+	# 	assert_equal(Array, isbn10_check(isbn10).class)
+	# end
+	# def test_that_isbn10_returns_an_array_of_10_elements
+	# 	isbn10 = "123456789x"
+	# 	assert_equal(10, isbn10_check(isbn10).count)
+	# end
+	def test_that_can_take_check_digit
+		isbn10 = "123456789x"
+		assert_equal("123456789", isbn10_check(isbn10))
+	end
 end
+
